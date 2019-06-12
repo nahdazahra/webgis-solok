de8fde268d806f12928b9af4e344a9383a9314bd
Nahda Fauziyah Zahra <znahda@gmail.com>
2019-06-12 19:39:36 +0700

    google on index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
+  <meta name="google-site-verification" content="n8I8xxDFhfhbbTnrfWDWqniSfLA5eN4SD8u44PBA6bQ" />
   <title>Peta Zona Nilai Tanah Kota Solok</title>
 
   <!-- Font Awesome Icons -->
